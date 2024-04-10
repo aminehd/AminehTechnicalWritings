4b57b1b8b329276e43fd5a86140a141555206693
aminehd <43146526+aminehd@users.noreply.github.com>
2024-04-10 12:32:00 -0700

    Update sampling_utils.py
    add set_xsamples method to Population sampler

--- src/sampling_utils.py
+++ src/sampling_utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-class ResponseSampler:
+class PopulationSampler:
     def __init__(self, population, seed=42):
         """
         Initializes the ResponseSampler with a given population and random seed.
@@ -11,17 +11,25 @@ class ResponseSampler:
         self.population = population
         self.rand_gen = np.random.default_rng(seed)
         self.n_repeat = len(population[:, 0]) // len(np.unique(population[:, 0]))
+        self.xs = []
 
-    def sample_response(self, x_sams):
+    def sample_response(self, x_sams=None):
         """
         Generates samples from the population based on the input x values.
 
         :param x_sams: A list or array of x indices for which to generate response samples.
         :return: A generator yielding tuples of (x, y) samples.
+
         """
+
+        if(not x_sams):
+            x_sams = self.xindeces
+            
         for x in x_sams:
             index = self.rand_gen.integers(0, self.n_repeat)
             yield (self.population[x * self.n_repeat + index, 0], self.population[x * self.n_repeat + index, 1])
+
+
 
     def get_samples(self, x_sams):
         """
@@ -31,8 +39,11 @@ class ResponseSampler:
         :return: A numpy array of (x, y) samples.
         """
         return np.array(list(self.sample_response(x_sams)))
-    def sample_xs(self, sample_size):
+    def set_sample_xs(self, sample_size):
         distinct_xs = np.unique(population[:, 0])
         x_samp_index = np.linspace(0, len(distinct_xs)-1, sample_size).astype(int)
         x_samples = distinct_xs[x_samp_index]
+        self.xindeces = x_samp_index 
+
+ 
         return (x_samp_index, x_samples)
