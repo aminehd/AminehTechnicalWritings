e898a54297dc801bcee2352b45306fa8607f3cce
aminehd <43146526+aminehd@users.noreply.github.com>
2024-04-11 17:18:41 -0700

    Update sampling_utils.py
    Fix

--- src/sampling_utils.py
+++ src/sampling_utils.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def getPopulation(file_path):
         files = np.load(file_path)
-        return files['arr_0']]
+        return files['arr_0']
         
         
 class PopulationSampler:
